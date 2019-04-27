1445ab3035e03c06053830c006cab0e269ded748
Dean <deanly.dev@gmail.com>
2019-04-27 19:59:47 +0900

    Update tomcat  - add deployment feature for gradle build

--- test/model.tomcat.test.ts
+++ test/model.tomcat.test.ts
@@ -3,22 +3,55 @@ import path from "path";
 
 import Tomcat from "../src/apps/Tomcat";
 
-const demoPomProject = Uri.file(path.join(__dirname, "..", "..", "test", "demo", "sample-java war-hello"));
+const demoPomProject1 = Uri.file(path.join(__dirname, "..", "..", "test", "demo", "sample-java war-hello"));
+const demoGradleProject1 = Uri.file(path.join(__dirname, "..", "..", "test", "demo", "joinfaces-gradle-war-example"));
+const demoGradleProject2 = Uri.file(path.join(__dirname, "..", "..", "test", "demo", "gradle-war"));
+
 const appTomcat8 = path.join(__dirname, "..", "..", "test", "apps", "apache-tomcat-8.5.28");
 
 suite("apps.tomcat", function () {
-    const tomcat = new Tomcat("test01", demoPomProject);
-    tomcat.setConfig({
-        id: "test01",
-        name: "test",
-        type: "TOMCAT",
-        appPath: appTomcat8,
-        properties: []
-    });
 
-
-    test("deploy", async function () {
+    test("deploy maven war 01", async function () {
+        const tomcat = new Tomcat("test01", demoPomProject1);
+        tomcat.setConfig({
+            id: "test01",
+            name: "test",
+            type: "TOMCAT",
+            appPath: appTomcat8,
+            properties: []
+        });
+        await tomcat.init();
         await tomcat.saveConfig();
         await tomcat.deploy();
     }).timeout(10000);
+
+    test("deploy gradle war 01", async function () {
+        const tomcat = new Tomcat("test01", demoGradleProject1);
+        tomcat.setConfig({
+            id: "test01",
+            name: "test",
+            type: "TOMCAT",
+            appPath: appTomcat8,
+            properties: []
+        });
+
+        await tomcat.init();
+        await tomcat.saveConfig();
+        await tomcat.deploy();
+    }).timeout(20000);
+
+    test("deploy gradle war 02", async function () {
+        const tomcat = new Tomcat("test01", demoGradleProject2);
+        tomcat.setConfig({
+            id: "test01",
+            name: "test",
+            type: "TOMCAT",
+            appPath: appTomcat8,
+            properties: []
+        });
+
+        await tomcat.init();
+        await tomcat.saveConfig();
+        await tomcat.deploy();
+    }).timeout(20000);
 });
